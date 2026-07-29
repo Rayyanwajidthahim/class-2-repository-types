d0e9bc5be25b95e6a66169d60abeb06c7202dcf3
Rayyanwajidthahim <amrayyanwajid@gmail.com>
2026-07-29 12:37:21 +0400

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,15 @@
+name=input("Enter your real agent name, good sir.")
+gadget=input(" please infrom us about your fav gadget in an deep, high stakes, undercover mission.")
+agent_number= 67
+speed_rating=43.84
+Height=1.89
+is_active=True
+stealth_rating=22.44
+print("name", name, "=type",type(name))
+print("gadget", gadget, "=type", type(gadget))
+print("agent_number", agent_number,"=type", type( agent_number))
+print("speed_rating", speed_rating,"=type", type(speed_rating))
+print("Height",Height , "=type", type(Height)) 
+print("is_active", is_active, "=type",type(is_active))
+print( "stealth_rating", stealth_rating, "=type", type(is_active))
+
---
+++ main.py
@@ -0,0 +1,11 @@
+print('HELLO')
+x=56
+z=71
+print (x+z)
+print (x-z)
+print(x/z)
+print('Problem Solved')
+input ("what is your name?")
+name= rayyan
+v=67
+print ("rayyan+v")
